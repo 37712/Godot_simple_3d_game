03f341c6def73fa4f8bb9187c1c7aefa05dab2d5
37712 <cgr37712@gmail.com>
2021-12-23 20:23:51 -0800

    added a mesh for the player ball and rolling effect

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,12 +8,9 @@ var max_speed = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	move(delta)
-	
-func move(_delta):
 	var input_vector = Vector3.ZERO
 	# better code, ok for keyboard and controller
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -31,9 +28,13 @@ func move(_delta):
 	else: # not moving
 		velocity = velocity.move_toward(Vector3.ZERO, friction)
 	
+	# rotate ball
+	rotate_x(deg2rad(velocity.z))
+	rotate_z(deg2rad(-velocity.x))
+	
 	velocity = move_and_slide(velocity) # collide and slide, no delta needed
-
-# clamped func for Vector3
+	
+# clamped func for Vector3, only works for (x,z) not y
 func clamped_3d(vect3, limit):
 	var vect2 = Vector2(vect3.x, vect3.z)
 	vect2 = vect2.clamped(limit)
